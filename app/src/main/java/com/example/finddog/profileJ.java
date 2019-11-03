d08a37e7eea9e7e3453aa446d7ca8eeba6c92477
Nattanan Semathong <60010313@kmitl.ac.th>
2019-11-03 15:37:30 +0700

    profile page connect to firebase

--- app/src/main/java/com/example/finddog/profileJ.java
+++ app/src/main/java/com/example/finddog/profileJ.java
@@ -6,10 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class profileJ extends AppCompatActivity implements View.OnClickListener{
 
@@ -17,6 +22,11 @@ public class profileJ extends AppCompatActivity implements View.OnClickListener{
 
     private TextView userEmail;
     private Button buttonLogout;
+
+    private DatabaseReference databaseReference;
+
+    private EditText editTextName, editTextPhone;
+    private Button buttonSaveInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +39,19 @@ public class profileJ extends AppCompatActivity implements View.OnClickListener{
             startActivity(new Intent(this, loginJ.class));
         }
 
+//        FirebaseUser checkUser = firebaseAuth.getCurrentUser();
+//        DatabaseReference  checkId =  databaseReference.child("user").child(checkUser.getUid());
+//        if (databaseReference.child("user").child(checkUser.getUid()) == checkId){
+//            finish();
+//            startActivity(new Intent(this, detaildogJava.class));
+//        }
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextPhone = (EditText) findViewById(R.id.editTextPhone);
+        buttonSaveInfo = (Button) findViewById(R.id.buttonSaveInfo);
+
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
         userEmail = (TextView) findViewById(R.id.textViewUserEmail);
@@ -36,7 +59,37 @@ public class profileJ extends AppCompatActivity implements View.OnClickListener{
 
         userEmail.setText("Welcome "+user.getEmail());
         buttonLogout.setOnClickListener(this);
+        buttonSaveInfo.setOnClickListener(this);
+
+
+
+        ImageView imggofind = findViewById(R.id.gofind);
+
+        imggofind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent (profileJ.this,detaildogJava.class);
+                startActivity(i);
+            }
+        });
+
+
     }
+
+    private void saveUserInformation(){
+        String name = editTextName.getText().toString().trim();
+        String phone = editTextPhone.getText().toString().trim();
+
+        UserInformation userInformation = new UserInformation(name,phone);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        databaseReference.child("user").child(user.getUid()).setValue(userInformation);
+
+        Toast.makeText(this, "Information Saved..", Toast.LENGTH_SHORT).show();
+
+    }
+
 
     @Override
     public void onClick(View v) {
@@ -45,6 +98,12 @@ public class profileJ extends AppCompatActivity implements View.OnClickListener{
             finish();
             startActivity(new Intent(this, loginJ.class));
         }
+
+        if (v == buttonSaveInfo){
+            saveUserInformation();
+        }
+
+
 
     }
 }
