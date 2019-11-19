d183804c5872f168d43fde544cba535fcb619dea
oat <oat10539@hotmail.com>
2019-11-19 17:37:43 +0700

    show  lost location

--- app/src/main/java/com/example/finddog/SingleMissing.java
+++ app/src/main/java/com/example/finddog/SingleMissing.java
@@ -2,20 +2,28 @@ package com.example.finddog;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.annotations.NotNull;
 import com.squareup.picasso.Picasso;
 
-public class SingleMissing extends AppCompatActivity {
+public class SingleMissing extends FragmentActivity implements OnMapReadyCallback  {
     private String mPost_key = null;
     private DatabaseReference databaseReference;
     private DatabaseReference reff;
@@ -28,11 +36,16 @@ public class SingleMissing extends AppCompatActivity {
     private TextView textTel;
     private TextView textOwner;
     private ImageView image;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.single_missing);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.showmap);
+        mapFragment.getMapAsync(this);
+
 
         databaseReference = FirebaseDatabase.getInstance().getReference().child("postmiss");
         mPost_key = getIntent().getExtras().getString("blog_id");
@@ -47,10 +60,12 @@ public class SingleMissing extends AppCompatActivity {
         textOwner = (TextView) findViewById(R.id.postOwner);
         image = (ImageView) findViewById(R.id.imgShow);
 
-         /*Toast.makeText(SingleMissing.this,mPost_key,Toast.LENGTH_SHORT).show();*/
-
-
-        databaseReference.child(mPost_key).addValueEventListener(new ValueEventListener() {
+        /*Toast.makeText(SingleMissing.this,mPost_key,Toast.LENGTH_SHORT).show();*/
+    }
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            mMap=googleMap;
+            databaseReference.child(mPost_key).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
@@ -60,6 +75,8 @@ public class SingleMissing extends AppCompatActivity {
                 String post_datetime =(String) dataSnapshot.child("datetime").getValue();
                 String post_prize =(String) dataSnapshot.child("prize").getValue();
                 String uid =(String) dataSnapshot.child("uid").getValue();
+                final Double latitude = dataSnapshot.child("Lat").getValue(Double.class);
+                final Double longitude = dataSnapshot.child("Lng").getValue(Double.class);
 
                 reff.child(uid).addValueEventListener(new ValueEventListener() {
                     @Override
@@ -68,6 +85,14 @@ public class SingleMissing extends AppCompatActivity {
                         String post_owner =(String) dataSnapshot.child("name").getValue();
                         textTel.setText(post_tel);
                         textOwner.setText(post_owner);
+//
+//                        Double latitude = dataSnapshot.child("Lat").getValue(Double.class);
+//                        Double longitude = dataSnapshot.child("Lng").getValue(Double.class);
+
+                        LatLng location = new LatLng(latitude, longitude);
+
+                        mMap.addMarker((new MarkerOptions().position(location)));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 14));
                     }
 
                     @Override
@@ -88,10 +113,13 @@ public class SingleMissing extends AppCompatActivity {
                 Picasso.get().load(post_img).into(image);
             }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
 
-            }
-        });
+                }
+
+
+            });
     }
-}
+    }
+
