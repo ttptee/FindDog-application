43a2df5c3fe2a4553a429b3c799c1a30d7ecd148
Tee <ttp.tee@hotmail.com>
2019-11-21 01:50:04 +0700

    app check post lat long
    not suss

--- app/src/main/java/com/example/finddog/checkPostBreed.java
+++ app/src/main/java/com/example/finddog/checkPostBreed.java
@@ -2,6 +2,7 @@ package com.example.finddog;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.Context;
@@ -19,27 +20,71 @@ import com.google.type.LatLng;
 
 import org.w3c.dom.Text;
 
-public class checkPostBreed extends AppCompatActivity {
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.widget.TextView;
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
+import android.util.Log;
+
+public class checkPostBreed extends Activity implements LocationListener {
+    private TextView textView;
+    private LocationManager locationManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_post_breed);
+
         SharedPreferences sp = getSharedPreferences("BreedText", Context.MODE_PRIVATE);
         String TextBreed = sp.getString("BreedMLText", "Error");
 
         TextView testBreed = findViewById(R.id.BreedText);
         testBreed.setText(TextBreed);
 
-
-
-
-
-
-
+        textView = (TextView) findViewById(R.id.Lat);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(checkPostBreed.this,Manifest.permission.ACCESS_FINE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED &&
+         ActivityCompat.checkSelfPermission(checkPostBreed.this,Manifest.permission.ACCESS_COARSE_LOCATION)
+                 != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    Activity#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        onLocationChanged(location);
 
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        double longitude=location.getLongitude();
+        double latitude = location.getLatitude();
+        textView.setText("longitude"+longitude+"\n"+"latitude"+latitude);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
